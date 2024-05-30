83f006583a9b30c309691734d69fd5d90a7c9c18
raditex <raditex@gmail.com>
2024-05-30 20:09:55 +0700

    upload dari vsc - 1

---
+++ dom.js
@@ -0,0 +1,10 @@
+// mengubah judul / title dokumen HTML
+document.title = "Beranda";
+document.title = "Home";
+
+// menulis ke dalam body browser
+document.write("Hallo..");
+document.write("<h1>Hallo Dunia..</h1>");
+
+// menghitung panjang / jumlah karakter dari judul dokumen html 
+document.title.length;
